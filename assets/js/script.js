526129c21687fcd98bbcc1da8b63d74dc9b8a9a8
Tony Stark <apurificato5680@gmai.com>
2024-04-15 01:25:02 -0400

    added some extra styling for sections that hold cards, as well as sections for headers

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
 
         $.each(form, function(index, taskObj) {
             formOutput.append(`
-                <div class="task row d-flex justify-content-center align-content-center align-center border border-dark p-3 rounded-3" draggable="true" style="background-color: #ffd700;">
+                <div class="task row d-flex justify-content-center align-content-center align-center border border-dark py-3 rounded-3" draggable="true" style="background-color: #ffd700; width: 75%">
                     <h3> ${taskObj.name} </h3>
                     <h5> ${taskObj.date} </h5>
                     <p> ${taskObj.description} </p>
