041465442a96153d963f8617a95cf62d698015a9
Tony Stark <apurificato5680@gmai.com>
2024-04-16 21:08:58 -0400

    updated javascript code to make sure delete buttons are working correctly. They are working as intended now.

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,11 @@ $(document).ready(function() {
     function getTask(e) {
         e.preventDefault();
 
-        const taskTitle = $('#taskTitle', addTaskForm).val();
-        const taskDueDate = $('#taskDueDate', addTaskForm).val();
-        const taskDescription = $('#taskDescription', addTaskForm).val();
+        const taskTitle = $('#taskTitle').val();
+        const taskDueDate = $('#taskDueDate').val();
+        const taskDescription = $('#taskDescription').val();
 
-        const formArray = JSON.parse(localStorage.getItem('addTaskForm')) || [];
+        const formArray = JSON.parse(localStorage.getItem('taskData')) || [];
 
         const formObj = {
             name: taskTitle,
@@ -20,78 +20,57 @@ $(document).ready(function() {
 
         formArray.push(formObj);
 
-        localStorage.setItem('addTaskForm', JSON.stringify(formArray));
+        localStorage.setItem('taskData', JSON.stringify(formArray));
 
         const modalInstance = bootstrap.Modal.getInstance(formModal[0]);
         modalInstance.hide();
 
         formModal.on('hidden.bs.modal', function () {
-            location.reload();
+            outputTask();
         });
     }
 
     function outputTask() {
-        const form = JSON.parse(localStorage.getItem('addTaskForm')) || [];
+        const form = JSON.parse(localStorage.getItem('taskData')) || [];
 
-        if (form.length) {
-            formOutput.html('');
-        }
+        formOutput.html('');
 
-        $.each(form, function(index, taskObj) {
+        form.forEach(taskObj => {
             formOutput.append(`
                 <div class="task row d-flex justify-content-center align-content-center align-center border border-dark py-3 rounded-3" draggable="true" droppable="true" style="background-color: #ffd700; width: 75%">
                     <h3> ${taskObj.name} </h3>
                     <h5> ${taskObj.date} </h5>
                     <p> ${taskObj.description} </p>
-                    <button class="delete-btn parentDiv" style="background-color: black; color: white; width: 50%;">Delete Task</button>
+                    <button class="delete-btn" style="background-color: black; color: white; width: 50%;">Delete Task</button>
                 </div>
             `);
         });
     }
 
-    function submit() {
-        addTaskForm.off('submit').on('submit', getTask);
-
-
+    function deleteTask() {
         $(document).on('click', '.delete-btn', function() {
-            const taskContainer = $(this).closest('.parentDiv');
-            taskContainer.remove('addTaskForm');
+            const taskContainer = $(this).closest('.task');
+            const index = taskContainer.index();
 
-            localStorage.removeItem('addTaskForm');
-            
+            let formArray = JSON.parse(localStorage.getItem('taskData')) || [];
+            formArray.splice(index, 1);
+            localStorage.setItem('taskData', JSON.stringify(formArray));
+
+            taskContainer.remove();
         });
-
-        // setupDrop();
     }
-
-    // function setupDrop() {
-    //     $('.task, .card-body').droppable({
-    //         accept: '.task',
-    //         drop: function (event, ui) {
-    //             $(this).append(ui.draggable.css({
-    //                 top: "auto",
-    //                 left: "auto"
-    //             }));
-    //         }
-    //     });
-    // }
 
     function setupDrop() {
-        // Select a more specific parent container if possible
-        $('.droppable-container').on('drop', '.task, .card-body', function droppableEvent(event, ui) {
-            // Append the draggable element to the droppable element
-            $(this).append(ui.draggable);
-            // Optionally, reset CSS properties if necessary
-            ui.draggable.css({
-                top: "auto",
-                left: "auto"
-            });
+        $('.droppable-container').droppable({
+            accept: '.task',
+            drop: function(event, ui) {
+                $(this).append(ui.draggable);
+            }
         });
     }
-    
 
-    outputTask();
-    submit();
+    addTaskForm.on('submit', getTask);
+    deleteTask();
     setupDrop();
-
+    outputTask(); // Initial task output
 });
